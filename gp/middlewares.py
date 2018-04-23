25d9fa2cb56750e8292e2336bdd257484d40ec7f
yehuanwen <yehuanwen@126.com>
2018-04-23 21:42:09 +0800

    增加注释

--- gp/middlewares.py
+++ gp/middlewares.py
@@ -13,14 +13,13 @@ class ChromeDownloaderMiddleware(object):
 
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')  # 设置无界面
         if CHROME_PATH:
             options.binary_location = CHROME_PATH
         if CHROME_DRIVER_PATH:
-            self.driver = webdriver.Chrome(chrome_options=options, executable_path=CHROME_DRIVER_PATH)
+            self.driver = webdriver.Chrome(chrome_options=options, executable_path=CHROME_DRIVER_PATH)  # 初始化Chrome驱动
         else:
-            self.driver = webdriver.Chrome(chrome_options=options)
+            self.driver = webdriver.Chrome(chrome_options=options)  # 初始化Chrome驱动
 
     def __del__(self):
         self.driver.close()
@@ -28,9 +27,9 @@ class ChromeDownloaderMiddleware(object):
     def process_request(self, request, spider):
         try:
             print('Chrome driver begin...')
-            self.driver.get(request.url)
+            self.driver.get(request.url)  # 获取网页链接内容
             return HtmlResponse(url=request.url, body=self.driver.page_source, request=request, encoding='utf-8',
-                                status=200)
+                                status=200)  # 返回HTML数据
         except TimeoutException:
             return HtmlResponse(url=request.url, request=request, encoding='utf-8', status=500)
         finally:
